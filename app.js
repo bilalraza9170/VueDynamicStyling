3c3d43019161a56f3ec12c69ad0859ad887fc957
bilal <bilalraza9170@gmail.com>
2024-06-25 12:14:13 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      boxA: false,
+      boxB: false,
+      boxC: false,
+    };
+  },
+  methods: {
+    box(box) {
+      if (box == "A") {
+        this.boxA = !this.boxA;
+      } else if (box == "B") {
+        this.boxB = !this.boxB;
+      } else if (box == "C") {
+        this.boxC = !this.boxC;
+      }
+    },
+  },
+});
+
+app.mount("#styling");
